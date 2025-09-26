e6bebc45be1b23eb164082aa5d648ae0d8b9321a
JJuandi14 <jdalferezs@eafit.edu.co>
2025-09-25 23:54:51 -0500

    Script del playermovement.cs

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,29 +1,63 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
-[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
     [Header("Movimiento")]
     public float moveSpeed = 5f;
-
+    public float turnSpeed = 200f;
     private Rigidbody2D rb;
-    private Vector2 movement;
+
+    [Header("UI")]
+    public Text scoreText;
+    public Text messageText;
+    private int score = 0;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        UpdateScoreUI();
+        messageText.text = "";
     }
 
     void Update()
-{
-    movement.x = Input.GetAxisRaw("Horizontal");
-    movement.y = Input.GetAxisRaw("Vertical");
-
-    Debug.Log("Movimiento: " + movement);
-}
-
-    void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        // Movimiento hacia adelante/atrás
+        float moveInput = Input.GetAxis("Vertical"); // W/S o flechas arriba/abajo
+        // Giro
+        float turnInput = Input.GetAxis("Horizontal"); // A/D o flechas izquierda/derecha
+
+        // Mover el carro
+        rb.linearVelocity = transform.up * moveInput * moveSpeed;
+        rb.angularVelocity = -turnInput * turnSpeed * Mathf.Deg2Rad;
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Energy")) // Rayitos de energía
+        {
+            score += 1;
+            UpdateScoreUI();
+            Destroy(other.gameObject);
+        }
+        else if(other.CompareTag("Banana")) // Cáscaras de banana
+        {
+            // Opcional: reiniciar nivel o reducir velocidad temporalmente
+            // Ejemplo: reiniciar nivel
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else if(other.CompareTag("Finish")) // Meta / línea de llegada
+        {
+            messageText.text = "¡Ganaste!";
+            rb.linearVelocity = Vector2.zero;
+            rb.angularVelocity = 0;
+            this.enabled = false; // Bloquear movimiento
+        }
+    }
+
+    void UpdateScoreUI()
+    {
+        scoreText.text = "Rayitos: " + score;
     }
 }
